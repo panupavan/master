30ec0c2ea1f8d41f36d9aaa3d095f4d315699132
panupavan <panupavan143@gmail.com>
2022-11-01 10:28:22 +0530

    1st

---
+++ src/main/java/com/panupavan/App.java
@@ -0,0 +1,13 @@
+package com.panupavan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
